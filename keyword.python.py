c0a44a5ca83f4387fa7d5b5cf42c35c6b648a3b1
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2022-12-03 01:11:11 +0530

    Add files via upload

---
+++ keyword.python.py
@@ -0,0 +1,11 @@
+Python3.10.6 (tags/v3.10.6:(c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import keyword
+print(keyword.kwlist)
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+print(keyword.iskeyword('False'))
+True
+print(keyword.iskeyword('if'))
+True
+print(keyword.iskeyword('KRUTIKA')
+      
